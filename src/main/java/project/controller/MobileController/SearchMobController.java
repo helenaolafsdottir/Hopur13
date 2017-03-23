8fc12bc2dcd9d39a7c8df984bbeec82f88850cc4
Sandra Gunnarsdottir <sandragunnarsdottir1@gmail.com>
2017-03-23 21:14:25 +0000

    GetFyrirSearchKomid
    get aðferð fyrir search

--- src/main/java/project/controller/MobileController/SearchMobController.java
+++ src/main/java/project/controller/MobileController/SearchMobController.java
@@ -2,10 +2,13 @@ package project.controller.MobileController;
 
 import java.util.List;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import project.persistence.entities.Recipe;
+import project.persistence.entities.User;
 import project.service.SearchService;
 
 @RestController
@@ -26,9 +30,27 @@ public class SearchMobController {
 		this.searchService = searchService;
 	}
 	
-	@RequestMapping(value="/m/search", method = RequestMethod.GET)
+	/*public Recipe parseRecipeData(String jsonString) throws JSONException {
+		System.out.println("jsonStr:  " + jsonString);
+	    JSONObject jsonObject = new JSONObject(jsonString);
+		Recipe recipe = new Recipe();
+		//formRecipe.setInstructions(jsonObject.getString("Instructions"));
+		recipe.setRecipeName(jsonObject.getString("recipeName"));
+		
+		return recipe;
+	}*/
+	
+	/*@RequestMapping(value="/m/search", method = RequestMethod.GET)
 	public @ResponseBody List<Recipe> search(@RequestParam("searchcond") String searchcond, @RequestParam("search") String search){
 		return searchService.findAllWithCond(searchcond, search);
+	}*/
+	
+	@RequestMapping(value="m/search", method = RequestMethod.GET)
+	public List<Recipe> search(@RequestParam("searchcond") String searchcond, @RequestParam("search") String search) throws JSONException{
+		
+		
+		return searchService.findAllWithCond(searchcond, search);
+		
 	}
 	
 	@RequestMapping(value="/m/search", method = RequestMethod.POST)
